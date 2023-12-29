12b52ec4ba346e46ce8ee34a8abd200b1ed962c5
Danny Summerlin <danny@summerlin.co>
2023-12-29 11:48:07 +0000

    don't load plugin if tinymce not in use

--- tinymce-plugin.js
+++ tinymce-plugin.js
@@ -1,33 +1,35 @@
 addEventListener("DOMContentLoaded", (e) => {
-	window.document.styleSheets[0].insertRule("i.mce-i-roughnotation { font: 400 20px/1 dashicons;padding: 0;vertical-align:top;margin-left: -2px;padding-right: 2px;}",  window.document.styleSheets[0].cssRules.length);
-	window.tinymce.PluginManager.add("rough-notation", function(ed) {
-		ed.addButton("rough-notation", {
-			name: 'rough-notation',
-			title: 'Rough Notation',
-			type: 'menubutton',
-			icon: 'roughnotation dashicons-marker',
-			menu: [{
-				text: 'Circle',
-				onclick: function() {
-					selected = ed.selection.getContent()
-					content = '[roughnotation type="circle" color="var(--)"]'+(selected || '')+'[/roughnotation] ';
-					ed.execCommand('mceInsertContent', false, content)
-				}
-			},{
-				text: 'Underline',
-				onclick: function() {
-					selected = ed.selection.getContent()
-					content = '[roughnotation type="underline" color="var(--)"]'+(selected || '')+'[/roughnotation] ';
-					ed.execCommand('mceInsertContent', false, content)
-				}
-			},{
-				text: 'Highlight',
-				onclick: function() {
-					selected = ed.selection.getContent()
-					content = '[roughnotation type="highlight" color="var(--)"]'+(selected || '')+'[/roughnotation] ';
-					ed.execCommand('mceInsertContent', false, content)
-				}
-			}]
+	if(window.tinymce) {
+		window.document.styleSheets[0].insertRule("i.mce-i-roughnotation { font: 400 20px/1 dashicons;padding: 0;vertical-align:top;margin-left: -2px;padding-right: 2px;}",  window.document.styleSheets[0].cssRules.length);
+		window.tinymce.PluginManager.add("rough-notation", function(ed) {
+			ed.addButton("rough-notation", {
+				name: 'rough-notation',
+				title: 'Rough Notation',
+				type: 'menubutton',
+				icon: 'roughnotation dashicons-marker',
+				menu: [{
+					text: 'Circle',
+					onclick: function() {
+						selected = ed.selection.getContent()
+						content = '[roughnotation type="circle" color="var(--)"]'+(selected || '')+'[/roughnotation] ';
+						ed.execCommand('mceInsertContent', false, content)
+					}
+				},{
+					text: 'Underline',
+					onclick: function() {
+						selected = ed.selection.getContent()
+						content = '[roughnotation type="underline" color="var(--)"]'+(selected || '')+'[/roughnotation] ';
+						ed.execCommand('mceInsertContent', false, content)
+					}
+				},{
+					text: 'Highlight',
+					onclick: function() {
+						selected = ed.selection.getContent()
+						content = '[roughnotation type="highlight" color="var(--)"]'+(selected || '')+'[/roughnotation] ';
+						ed.execCommand('mceInsertContent', false, content)
+					}
+				}]
+			})
 		})
-	})
+	}
 })
