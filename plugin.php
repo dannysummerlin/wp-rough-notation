9993b07a74d8398dcdfaabac134699a683c6a360
Danny Summerlin <danny@summerlin.co>
2023-12-29 12:20:36 +0000

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -14,10 +14,12 @@ add_action('wp_head', function () {
 		<script src="https://unpkg.com/rough-notation@0.5.1/lib/rough-notation.iife.js"></script>
 		<style> mark.rough-notation {background:none;display:inline-block;position:relative;color:unset} </style>
 		<script>
+let activeRoughNotations = []
+window.addEventListener("drawRoughNotation", () => { activeRoughNotations.forEach(e=>e.show()) })
 document.addEventListener('readystatechange', ()=>{
 	if(document.readyState != 'complete')
 		return
-	document.querySelectorAll('.rough-notation').forEach(e=>RoughNotation.annotate(e, {
+	document.querySelectorAll('.rough-notation').forEach(e=>activeRoughNotations.push(RoughNotation.annotate(e, {
 		"type": e.dataset.type || null,
 		"animate": e.dataset.animate || true,
 		"animationDuration": (e.dataset.animationDuration || 800) + "ms",
@@ -27,8 +29,9 @@ document.addEventListener('readystatechange', ()=>{
 		"multiline": e.dataset.multiline || true,
 		"iterations": e.dataset.iterations || 1,
 		"brackets": e.dataset.brackets || 'top',
-	}).show()
-)})
+	})))
+	window.dispatchEvent(new Event("drawRoughNotation"))
+})
 		</script>
 <?php
 });
